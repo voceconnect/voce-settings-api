c5091b139ddc9117717016d968690e926f7098d0
= <mpretty@voceconnect.com>
2012-02-28 10:37:03 -0500

    adding vs_sanitize_email sanitization function

--- sanitize-callbacks.php
+++ sanitize-callbacks.php
@@ -1,13 +1,49 @@
 <?php
-
+/**
+ *
+ * @param variable $value
+ * @param Voce_Setting $setting
+ * @param array $args 
+ * @return variable
+ */
 function vs_santize_checkbox($value, $setting, $args) {
 	return !is_null($value);
 }
 
+/**
+ *
+ * @param variable $value
+ * @param Voce_Setting $setting
+ * @param array $args 
+ * @return variable
+ */
 function vs_santize_text($value, $setting, $args) {
 	return trim(strip_tags($value));
 }
 
+/**
+ *
+ * @param variable $value
+ * @param Voce_Setting $setting
+ * @param array $args 
+ * @return variable
+ */
 function vs_sanitize_url($value, $setting, $args) {
 	return esc_url_raw($value);
+}
+
+/**
+ *
+ * @param variable $value
+ * @param Voce_Setting $setting
+ * @param array $args 
+ * @return variable
+ */
+function vs_sanitize_email($value, $setting, $args) {
+	$value = trim($value);
+	if(!is_email( $value )) {
+		$setting->add_error(sprintf('The %s is not a valid email address', $setting->title));
+		return null;
+	}
+	return $value;
 }
