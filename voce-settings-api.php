3ff63b5f95bb5db15dc23fbc24c7f40128fd5327
Kevin Langley <me@kevinlangleyjr.com>
2013-11-07 09:06:55 -0500

    changing how were enqueueing scripts and styles by only doing it on load-$pagehook

--- voce-settings-api.php
+++ voce-settings-api.php
@@ -150,9 +150,9 @@ class Voce_Settings_Page {
 				$page_hook = add_menu_page($this->title, $this->menu_title, $this->capability, $this->page_key, array($this, 'display'));
 			}
 
-			add_action( 'admin_enqueue_scripts', function( $current_hook ) use ( $page_hook ){
-				if( $current_hook == $page_hook )
-					do_action( 'vs_admin_enqueue_scripts', $this, $page_hook );
+			add_action( 'load-' . $page_hook, function(){
+				do_action( 'vs_admin_enqueue_scripts', $this );
+
 			} );
 		}
 	}
